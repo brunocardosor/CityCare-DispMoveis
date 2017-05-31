f5ae0b0979d0389e31fff64eaa181b9d699141b4
lab1-18 <brun.oana@hotmail.com>
2017-05-30 21:37:37 -0300

    ...

---
+++ app/src/main/java/br/edu/leaosampaio/CityCare/Activity/DenunciaActivity.java
@@ -0,0 +1,21 @@
+package br.edu.leaosampaio.CityCare.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.edu.leaosampaio.CityCare.R;
+import br.edu.leaosampaio.citycare_dispmoveis.R;
+
+public class DenunciaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_denuncia);
+
+        getActionBar().setTitle("Denuncia");
+
+
+
+    }
+}
---
+++ app/src/main/java/br/edu/leaosampaio/CityCare/Modelo/Usuario.java
@@ -0,0 +1,79 @@
+package br.edu.leaosampaio.CityCare.Modelo;
+import android.text.Editable;
+
+/**
+ * Created by lenilson on 21/04/17.
+ */
+
+public class Usuario {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String senha;
+    private String estado;
+    private String cidade;
+    private boolean status;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String toString(){
+        return this.nome;
+    }
+
+}
